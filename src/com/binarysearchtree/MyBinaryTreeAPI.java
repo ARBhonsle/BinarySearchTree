0585a3bf02e414af2461f983c36db6c594884fec
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-25 19:20:33 +0530

    Added: Project files

---
+++ src/com/binarysearchtree/MyBinaryTreeAPI.java
@@ -0,0 +1,12 @@
+package com.binarysearchtree;
+
+/**
+ * MyBinarySearchTreeAPI class
+ * Binary Search Tree methods executed
+ */
+public class MyBinaryTreeAPI {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree API");
+    }
+}

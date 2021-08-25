d4e88d4259568176d37a536c04729587713dcff4
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-25 20:07:24 +0530

    Refactor: New Binary tree created

--- src/com/binarysearchtree/MyBinaryTreeAPI.java
+++ src/com/binarysearchtree/MyBinaryTreeAPI.java
@@ -12,6 +12,16 @@ public class MyBinaryTreeAPI {
         binaryTree.addNode(56);
         binaryTree.addNode(30);
         binaryTree.addNode(70);
+        binaryTree.addNode(22);
+        binaryTree.addNode(40);
+        binaryTree.addNode(11);
+        binaryTree.addNode(3);
+        binaryTree.addNode(16);
+        binaryTree.addNode(60);
+        binaryTree.addNode(65);
+        binaryTree.addNode(63);
+        binaryTree.addNode(67);
+        binaryTree.addNode(95);
         int size = binaryTree.size;
         System.out.println("Size of Binary tree: "+size);
         binaryTree.printTree();
